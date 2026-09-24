b03ea5c87ea774b14f9b897cdb2fa0b392f5f323
Anushka Kedari <anushkakedari20@gmail.com>
2026-09-24 20:08:11 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> mp;
+        
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        for(char c : s){
+            mp[c]++;
+        }
+
+        for(char c : t){
+            mp[c]--;
+        }
+
+        for(auto pair : mp){
+            if(pair.second != 0){
+                return false;
+            }
+        }
+    return true;
+    }
+};
